56d77ea63016cb4620f4ee51e2d76b3ab72c8cf1
Chen <chen.jiang.1206@gmail.com>
2023-05-01 15:46:49 -0400

    done

--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -2,6 +2,15 @@
 
 from not_none_functions import return_not_none
 
-# def test_return_not_none():
-#     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
-#     assert False
+def test_return_not_none():
+    '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
+    assert type(return_not_none()) != None
+
+
+# def test_return_string():
+#     '''in string_functions, function "return_string()" returns a variable of type str.'''
+#     assert type(return_string()) == str
+
+# def test_interpolate_string():
+#     '''in string_functions, function "interpolate_string()" takes a string and inserts it into another string.'''
+#     assert interpolate_string('Guido') == 'Hello, Guido!'
